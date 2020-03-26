b585ea361f04c64a99bc3969e9a829e4682d127f
Deep Desai <did29@drexel.edu>
2020-03-26 19:10:52 -0400

    First try

---
+++ ProjectEulerTests/Program2to20Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace ProjectEulerTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using ProjectEuler;
+    using System;
+    using Xunit;
+
+    [TestClass]
+    public class Program2to20Tests
+    {
+        [DataTestMethod]
+        [DataRow(10, 3)]
+        [DataRow(11, 6)]
+        [DataRow(100, 10)]
+        [DataRow(110, 16)]
+        [DataRow(111, 19)]
+        [DataRow(121, 22)]
+        [DataRow(900, 11)]
+        [DataRow(51, 8)]
+        [DataRow(151, 21)]
+        [DataRow(841, 23)]
+        [DataRow(99, 10)]
+        [DataRow(911, 20)]
+        [DataRow(999, 24)]
+        [DataRow(1000, 11)]
+        public void NumberLetterCountTest(int inputValue, int expectedValue)
+        {
+            Assert.AreEqual(Program2to20.NumberLetterCount(inputValue), expectedValue);
+        }
+
+        [DataTestMethod]
+        [DataRow(10, 39)]
+        public void AllNumberLetterCountFactTest(int inputValue, int expectedValue)
+        {
+            Assert.AreEqual(Program2to20.AllNumberLetterCount(inputValue), expectedValue);
+        }
+    }
+}
---
+++ ProjectEulerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectEulerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectEulerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d1914f3-6172-468e-9f82-bafda1cb3f98")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
